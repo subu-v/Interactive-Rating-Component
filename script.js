4aa3de743048fb2c083bc2f03808c0f48632fa9f
subramani <106590803+subu-v@users.noreply.github.com>
2022-06-15 10:41:47 +0530

    Update script.js
    Added the functionality required for the submitting rating and displaying them in the thank-you state ui

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-const sumbitButton = document.querySelector(".rating-component__button");
+const submitButton = document.querySelector(".rating-component__button");
 const ratingComponent = document.querySelector(".rating-component");
 const thankYouComponent = document.querySelector(".thank-you");
+const ratings = document.querySelectorAll(".rating-component__number");
+const receivedRating = document.querySelector(".submitted-rating");
+let submittedRating;
 
-sumbitButton.addEventListener("click", function () {
+for (let i = 0; i < ratings.length; i++) {
+  ratings[i].addEventListener("click", function () {
+    submittedRating = ratings[i].textContent;
+    receivedRating.textContent = submittedRating;
+  });
+}
+
+submitButton.addEventListener("click", function () {
   ratingComponent.classList.toggle("hidden");
   thankYouComponent.classList.toggle("hidden");
 });
